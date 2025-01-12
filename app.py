3c86af9492b4db73cd45156a0f71bff67b8a5bf6
claytonbrgs <142549801+claytonbrgs@users.noreply.github.com>
2025-01-12 12:12:29 -0300

    price range - slider

--- app.py
+++ app.py
@@ -4,5 +4,16 @@ import pandas as pd
 #page configuration
 st.set_page_config(layout='wide')
 
+#csv variables
 df_reviews = pd.read_csv("dataset/customer-reviews.csv")
-df_top100_books = pd.read_csv("datasets/top-100-trending-books.csv")
+df_top_100_books = pd.read_csv("dataset/top-100-trending-books.csv")
+
+#price range - slider
+price_max = df_top_100_books["book price"].max()
+price_min = df_top_100_books["book price"].min()
+
+max_price = st.sidebar.slider("Price range", price_min, price_max, price_max)
+
+df_top_100_books_price_range_filter = df_top_100_books[df_top_100_books["book price"] <= max_price]
+
+df_top_100_books_price_range_filter
