4b7df1ef5b712578391cfa9da28354e8366043d1
claytonbrgs <142549801+claytonbrgs@users.noreply.github.com>
2025-01-12 11:55:21 -0300

    csv variables defined

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ import pandas as pd
 
 #page configuration
 st.set_page_config(layout='wide')
+
+df_reviews = pd.read_csv("dataset/customer-reviews.csv")
+df_top100_books = pd.read_csv("datasets/top-100-trending-books.csv")
